01ef5f34e449ae18641617ca1c5dbd9a1b60df93
Остап <141727472+oostap2@users.noreply.github.com>
2025-12-31 14:14:26 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -5,62 +5,83 @@ from random import randint
 
 pygame.mixer.init()
 
-BMP = 140
-BEAT_TIME = 60 / BMP
-BAR_TIME = BEAT_TIME * 4
-QUATER_BEAT_TIME = BEAT_TIME / 4
-
-
-def getRandomNote() -> int:
-    return randint(0, 6)
-
+BPM = 140
+BEAT_TIME = 60 / BPM
+notes = ['C', 'D', 'E', 'F', 'G', 'A', 'B']
 
 class Bar:
-    def __init__(self):
-        self.bass = getRandomNote()
-        self.note1 = self.getFirstNote()
-        self.note2 = getRandomNote()
-        self.note3 = getRandomNote()
-        
     def getFirstNote(self) -> int:
-        if self.bass >= 5:
-            return self.getNoteIndex(0)
-        if self.bass >= 3:
-            return self.getNoteIndex(1)
+        if self.bass >= 5: return self.getNoteIndex(0)
+        if self.bass >= 3: return self.getNoteIndex(1)
         return self.getNoteIndex(2)
 
     def getNoteIndex(self, n: int) -> int:
         return self.bass + 2 * randint(0, n)
 
+    def getRandomNote(self) -> int:
+        return randint(0, 6)
+
+    def describe(self):
+        print('bass', notes[self.bass] + '3')
+        melody = ''
+        for note in [self.note1, self.note2, self.note3, self.note4]:
+            melody += notes[note] + '4 '
+        print('melody', melody + '\n')
+
+
+class SmoothBar(Bar):
+    def __init__(self):
+        self.bass = self.getRandomNote()
+        self.note1 = self.getFirstNote()
+        self.note2 = self.getStepNote(self.note1)
+        self.note3 = self.getStepNote(self.note2)
+        self.note4 = self.getStepNote(self.note3)
+
+    def getStepNote(self, prev_note: int) -> int:
+        step = randint(-1, 1)
+        new_note = prev_note + step
+        return max(0, min(new_note, 6))
+
+class RandomBar(Bar):
+    def __init__(self):
+        self.bass = self.getRandomNote()
+        self.note1 = self.getFirstNote()
+        self.note2 = self.getRandomNote()
+        self.note3 = self.getRandomNote()
+        self.note4 = self.bass
+
 
 class Program:
     def __init__(self):
-        self.notes = ['C', 'D', 'E', 'F', 'G', 'A', 'B']
-
         self.bass = []
         self.melody = []
         self.addNotesToList(self.bass, 'sound/bass/{}3.ogg')
         self.addNotesToList(self.melody, 'sound/melody/{}4.ogg')
         
-    def addNotesToList(self, list: list, path: str):
-        for note in self.notes:
-            list.append(pygame.mixer.Sound(path.format(note)))
+    def addNotesToList(self, note_list: list, path: str):
+        for note in notes:
+            note_list.append(pygame.mixer.Sound(path.format(note)))
 
     def playBar(self, bar: Bar):
-        self.playAndWait(self.bass[bar.bass], QUATER_BEAT_TIME)
-        self.playAndWait(self.melody[bar.note1], BEAT_TIME)
-        self.playAndWait(self.melody[bar.note2], BEAT_TIME)
-        self.playAndWait(self.melody[bar.note3], BEAT_TIME)
-        self.playAndWait(self.melody[bar.bass], QUATER_BEAT_TIME * 3)
+        self.bass[bar.bass].play()
+        self.playAndWait(self.melody[bar.note1])
+        self.playAndWait(self.melody[bar.note2])
+        self.playAndWait(self.melody[bar.note3])
+        self.playAndWait(self.melody[bar.note4])
     
-    def playAndWait(self, sound: pygame.mixer.Sound, waiting_time: float):
+    def playAndWait(self, sound: pygame.mixer.Sound):
         sound.play()
-        time.sleep(waiting_time)
+        time.sleep(BEAT_TIME)
 
 
 if __name__ == '__main__':
     instance = Program()
 
     print('Press Ctrl+C to end the program.')
-    while True:
-        instance.playBar(Bar())
+    try:
+        while True:
+            bar = SmoothBar()
+            bar.describe
+            instance.playBar(bar)
+    except:
+        pass
